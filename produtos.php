8f0001ab550a72a838821e01dce77eeb1a56a2e1
thiaraujo07 <89668004+thiaraujo07@users.noreply.github.com>
2021-08-27 20:15:49 -0300

    Add files via upload

---
+++ produtos.php
@@ -0,0 +1,147 @@
+<div id="imagemPaginaPrincipal">
+	<p>INTEGRAIS</p>
+	<div class="imgPequena">
+		<div class="imgProduto">
+			<img src="images/produto1.PNG"><br>			
+		</div>
+		<h2>R$: 5,75</h2>
+		<a href="index.php?pg=5&cod=0"> <p class="comprar"> COMPRAR</p></a>
+		
+		
+	</div>
+
+	<div class="imgPequena">
+		<div class="imgProduto">
+			<img src="images/produto2.PNG"><br>
+			
+		</div>
+		<h2>R$: 7,45</h2>
+		<a href="index.php?pg=5&cod=1">  <p class="comprar"> COMPRAR</p></a>		
+		
+	</div>
+
+	<div class="imgPequena">
+		<div class="imgProduto">
+			<img src="images/produto3.PNG"><br>
+		</div>
+		<h2>R$: 3,25</h2>
+		<a href="index.php?pg=5&cod=2">  <p class="comprar"> COMPRAR</p></a>
+		
+	</div>
+
+	<div class="imgPequena">
+		<div class="imgProduto">
+			<img src="images/produto4.PNG"><br>
+			
+		</div>
+		<h2>R$: 5,90</h2>
+		<a href="index.php?pg=5&cod=3">  <p class="comprar"> COMPRAR</p></a>		
+		
+	</div>
+	<div class="imgPequena">
+		<div class="imgProduto">
+			<img src="images/produto5.PNG"><br>
+			
+		</div>
+		<h2>R$: 10,50</h2>
+		<a href="index.php?pg=5&cod=4"><p class="comprar"> COMPRAR</p></a>
+		
+	</div>
+	<hr><br>
+	<p>FRUTAS E FARINHAS</p>
+	<div class="imgPequena">
+		<div class="imgProduto">
+			<img src="images/produto6.PNG"><br>
+			
+		</div>
+		<h2>R$: 9,90</h2>
+		<a href="index.php?pg=5&cod=5">  <p class="comprar"> COMPRAR</p></a>		
+		
+	</div>
+
+	
+	<div class="imgPequena">
+		<div class="imgProduto">
+			<img src="images/produto7.PNG"><br>
+		</div>
+		<h2>R$: 15,30</h2>
+		<a href="index.php?pg=5&cod=6">  <p class="comprar"> COMPRAR</p></a>
+		
+	</div>
+	<div class="imgPequena">
+		<div class="imgProduto">
+			<img src="images/produto8.PNG"><br>
+		</div>
+		<h2>R$: 25,70</h2>
+		<a href="index.php?pg=5&cod=7">  <p class="comprar"> COMPRAR</p></a>
+		
+	</div>
+	<div class="imgPequena">
+		<div class="imgProduto">
+			<img src="images/produto1.PNG"><br>			
+		</div>
+		<h2>R$: 5,75</h2>
+		<a href="index.php?pg=5&cod=0"> <p class="comprar"> COMPRAR</p></a>
+		
+		
+	</div>
+
+	<div class="imgPequena">
+		<div class="imgProduto">
+			<img src="images/produto2.PNG"><br>
+			
+		</div>
+		<h2>R$: 7,45</h2>
+		<a href="index.php?pg=5&cod=1">  <p class="comprar"> COMPRAR</p></a>		
+		
+	</div>
+	
+	<br><br>
+	<p>DOCES</p>
+	<div class="imgPequena">
+		<div class="imgProduto">
+			<img src="images/produto4.PNG"><br>
+			
+		</div>
+		<h2>R$: 5,90</h2>
+		<a href="index.php?pg=5&cod=3">  <p class="comprar"> COMPRAR</p></a>		
+		
+	</div>
+	<div class="imgPequena">
+		<div class="imgProduto">
+			<img src="images/produto5.PNG"><br>
+			
+		</div>
+		<h2>R$: 10,50</h2>
+		<a href="index.php?pg=5&cod=4"><p class="comprar"> COMPRAR</p></a>
+		
+	</div>
+	<div class="imgPequena">
+		<div class="imgProduto">
+			<img src="images/produto6.PNG"><br>
+			
+		</div>
+		<h2>R$: 9,90</h2>
+		<a href="index.php?pg=5&cod=5">  <p class="comprar"> COMPRAR</p></a>		
+		
+	</div>
+
+	
+	<div class="imgPequena">
+		<div class="imgProduto">
+			<img src="images/produto7.PNG"><br>
+		</div>
+		<h2>R$: 15,30</h2>
+		<a href="index.php?pg=5&cod=6">  <p class="comprar"> COMPRAR</p></a>
+		
+	</div>
+	<div class="imgPequena">
+		<div class="imgProduto">
+			<img src="images/produto8.PNG"><br>
+		</div>
+		<h2>R$: 25,70</h2>
+		<a href="index.php?pg=5&cod=7">  <p class="comprar"> COMPRAR</p></a>
+		
+	</div>
+	
+</div>
